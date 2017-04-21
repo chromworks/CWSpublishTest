1b2bda5a6f994cffe8b02cb375140aee38386103
Jay Yun <jay1.yun@gmail.com>
2017-04-21 11:04:10 -0400

    delete

--- copyfiles.bat
+++
@@ -1,2 +0,0 @@
-copy C:\Users\JYun\Dropbox\_Dev\CWS\src\CWS\bin\Release\CWS.exe C:\Users\JYun\AppData\Local\CWS\app-17.4.21\CWS.exe
-copy C:\Users\JYun\Dropbox\_Dev\CWS\src\CWS\bin\Release\update.dll C:\Users\JYun\AppData\Local\CWS\app-17.4.21\update.dll
